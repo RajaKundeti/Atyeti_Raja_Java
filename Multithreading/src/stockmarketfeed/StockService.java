6b69a883830c4acfcf861ed0b708fdba75852670
RajaKundeti <raja.kundeti@atyeti.com>
2025-06-23 16:19:41 +0530

    updated stock application and added spikes and their infos

--- Multithreading/src/stockmarketfeed/StockService.java
+++ Multithreading/src/stockmarketfeed/StockService.java
@@ -7,10 +7,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
@@ -23,12 +20,12 @@ public class StockService {
         Optional<Stock> min = stockList.stream().min((s1, s2) -> (int) (s1.getPrice() - s2.getPrice()));
         if (min.isPresent()) {
             Stock stock = min.get();
-            minMaxStocks.put("Min Stock",stock);
+            minMaxStocks.put("Min Stock (in Price)",stock);
         }
         Optional<Stock> max = stockList.stream().max((s1, s2) -> (int) (s1.getPrice() - s2.getPrice()));
         if (max.isPresent()) {
             Stock stock = max.get();
-            minMaxStocks.put("Max Stock",stock);
+            minMaxStocks.put("Max Stock (in Price)",stock);
         }
         return minMaxStocks;
     }
@@ -38,7 +35,7 @@ public class StockService {
         stockList.stream().collect(Collectors.groupingBy(Stock::getSymbol)).entrySet().forEach(entry->{
         String symbol = entry.getKey();
         List<Stock> stocks = entry.getValue();
-        Path filePath = Paths.get("C:\\Users\\RajaNarasimhanKundet\\OneDrive - Atyeti Inc\\Desktop\\Self\\JavaPOC\\Atyeti_Raja_Java\\Multithreading\\src\\stockmarketfeed\\stocksdataperstock_" + symbol + ".csv");
+        Path filePath = Paths.get("C:\\Users\\RajaNarasimhanKundet\\OneDrive - Atyeti Inc\\Desktop\\Self\\JavaPOC\\Project_Training\\Multithreading\\src\\stockmarketfeed\\stocksdataperstock\\" + symbol + ".csv");
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath.toFile()))){
             writer.write("Timestamp,Symbol,Price,Volume");
             writer.newLine();
@@ -51,5 +48,38 @@ public class StockService {
                 throw new RuntimeException(e);
             }
         });
+    }
+
+
+    public Map<String, List<SpikeInfo>> detectSpikes(List<Stock> stockList) {
+        Map<String, List<SpikeInfo>> spikeMap = new HashMap<>();
+        stockList.stream()
+                .collect(Collectors.groupingBy(Stock::getSymbol, Collectors.toList()))
+                .forEach((symbol, stocks) -> {
+
+                    List<Stock> sortedStocks = stocks.stream()
+                            .sorted(Comparator.comparing(Stock::getTimestamp)).toList();
+
+                    double minPrice = Double.MAX_VALUE;
+                    Stock baseStock = null;
+
+                    for (Stock stock : sortedStocks) {
+                        if (stock.getPrice() > minPrice * 1.10) {
+                            SpikeInfo info = new SpikeInfo(
+                                    symbol,
+                                    stock.getTimestamp(),
+                                    stock.getPrice(),
+                                    baseStock.getPrice()
+                            );
+                            spikeMap.computeIfAbsent(symbol, k -> new ArrayList<>()).add(info);
+                        }
+
+                        if (stock.getPrice() < minPrice) {
+                            minPrice = stock.getPrice();
+                            baseStock = stock;
+                        }
+                    }
+                });
+        return spikeMap;
     }
 }
